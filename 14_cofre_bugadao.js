c6a79185f0625389fb1afa3a04305c6a7911899b
Thiago Porto <porthiago@gmail.com>
2022-03-09 23:49:24 -0300

    update_finalizado

--- 14_cofre_bugadao.js
+++ 14_cofre_bugadao.js
@@ -3,74 +3,35 @@ function processData(input) {
     const splittedInput = input.trim().split('\n');
 
     const senha = splittedInput[0];
-    const senhaDigitada = splittedInput[1];
+    const senhaRecebida = splittedInput[1];
 
-    // console.log(splittedInput);
-    // console.log(senhaCorreta)
-    // console.log(senhaDigitada)
+    let guardaSequencia = '';
+    let verificador = senhaRecebida;
+    let contador = 0;
 
-    let guardaSequencia = [];
+    for (let i = 0; i < senha.length; i++) {
 
-    //a merda ta aqui
-    // for (let letraDaSenha of senha) {
+        for (let j = 0; j < verificador.length; j++) {
 
-    //     for (let letraDigitada of senhaDigitada) {
+            if (senha[i] === verificador[j]) {
+                guardaSequencia += verificador[j]
+                verificador = verificador.slice(verificador.indexOf(senha[i]));
 
-    //         if (letraDaSenha === letraDigitada) {
-    //             contador++
-    //         }
-    //     }
+                break;
+            } else {
 
 
-    for (let letra of senha) {
-        // if (senhaDigitada.includes(letra)) {
-
-        if (guardaSequencia.length < senha.length) {
-            guardaSequencia.push(senhaDigitada.indexOf(letra))
-            continue;
-        }
-        //}
-    }
-
-    //}
-
-    // if (contador >= senha.length) {
-    //     console.log('SIM');
-    // } else {
-    //     console.log('NAO');
-    // }
-
-    //guardaSequencia = guardaSequencia.join('');
-
-    let guardaValue;
-
-    for (let value of guardaSequencia) {
-        if (guardaValue === value) {
-
-        }
-    }
-
-    let contaSim = 0;
-
-    for (let i = 0; i < guardaSequencia.length; i++) {
-
-        if (guardaSequencia[i] < guardaSequencia[i + 1] && !guardaSequencia.includes(-1)) {
-            contaSim++
+            }
         }
 
     }
 
-    // console.log(contaSim)
-    // console.log(senha.length)
-    if (contaSim === senha.length - 1) {
-        console.log("SIM")
-    } else {
-        console.log("NAO")
-    }
 
-    console.log(guardaSequencia)
-
+    senha === guardaSequencia && verificador !== senhaRecebida ? console.log('SIM') : console.log('NAO');
 }
 
 
-processData('xxCcubBos\nxxxxCticgujbpBqionabos\n')
+processData('cubos\nxcxuxbxoxs\n') //SIM
+processData('cubos\nsuboc\n') //NAO
+processData('cubos\newvelrabsocaeln\n') //NAO
+
